c7da8c89c3d7dca21fc81ba3c164f6e7c9a20229
Manish Soni <100ni.manish@gmail.com>
2017-05-24 12:40:09 +0530

    souce code
    souce code

--- test/compass_test.rb
+++ test/compass_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 
+
 class CompassTest < Minitest::Test
   def test_create_project
     command = 'rm -rf tmp/new-compass-project; bundle exec compass create tmp/new-compass-project -r bootstrap-sass --using bootstrap --trace --force'
